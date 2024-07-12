6d2666c99de2e80142e4e922ee566152a0471605
L-mj0 <1047353451@qq.com>
2024-07-12 20:48:01 +0800

    just a test

--- src/main.c
+++ src/main.c
@@ -8,8 +8,11 @@
 #include <linux/input.h>
 #include "show_bmp.h"
 #include "touch.h"
+#include <stdlib.h>
 
-int x, y, ts;
+// #define SWIPE_THRESHOLD 50 // 定义滑动的最小距离阈值
+
+int x, y, ts,startX, endX, startY, endY;; 
 int val = 0;
 struct input_event touch;
 void left()
@@ -31,8 +34,10 @@ void show()
     if(val == 2) show_bmp("3.bmp");
     sleep(1);
 }
+
 int main()
 {
+    
     init_lcd();
     ts = init_EV_ABS();
     show();
@@ -41,35 +46,39 @@ int main()
         read(ts, &touch, sizeof(touch));
         if(touch.type == EV_ABS && touch.code == ABS_X) x = touch.value * 800 / 1024;
         if(touch.type == EV_ABS && touch.code == ABS_Y) y = touch.value * 480 / 600;
-        if(touch.type == EV_KEY && touch.code == BTN_TOUCH) // 判断压力值
+        if (touch.type == EV_KEY && touch.code == BTN_TOUCH) // 判断压力值
         {
-            if(touch.value == 0)
+            if (touch.value == 1) // 当压力值为1时，其实就是按下的时候
             {
-                if(x < 400)
+                startX = x;
+                startY = y;
+            }
+            if (touch.value == 0) // 当压力值为0时，其实就是松手的时候
+            {
+                endX = x;
+                endY = y;
+                printf("startX=%d, startY=%d, endX=%d, endY=%d\n", startX, startY, endX, endY);
+
+                int deltaX = endX - startX;
+                int deltaY = endY - startY;
+
+                if (abs(deltaX) > abs(deltaY) && abs(deltaX) > SWIPE_THRESHOLD) // 判断滑动方向
                 {
-                    left();
-                    show();
-                    printf("left\n");
-                }
-                else
-                {
-                    if(x > 750 && y > 430)
+                    if (deltaX < 0)
                     {
-                        for(int i=1;i<=3;i++)
-                        {
-                            left();
-                            show();
-                        }
+                        printf("左滑\n");
+                        left();
                     }
                     else
                     {
+                        printf("右滑\n");
                         right();
-                        show();
                     }
-                    printf("right\n");
+                    show();
                 }
             }
         }
+
     }
     close_EV_ABS();
     close_lcd();
